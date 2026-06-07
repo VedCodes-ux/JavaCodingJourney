b7525df4ceb90916ee36ed485f94501cc76a8344
VedCodes-ux <sablaniaved51@gmail.com>
2026-06-07 15:21:25 +0530

    learned quick sort

---
+++ quicksort.java
@@ -0,0 +1,45 @@
+package JavaCodingJourney;
+
+public class quicksort {
+    public static int partition(int [] arr,int si ,int ei) {
+        int pivot = arr[ei];
+        int i = si-1;
+        
+        for (int j = si; j < ei; j++) {
+            if(arr[j]<pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[ei];
+        arr[ei] = temp;
+
+        return i;
+    }
+
+    public static void quicksort(int [] arr , int si, int ei) {
+
+        if(si>=ei) {
+            return;
+        }
+        
+        int pidx = partition(arr, si, ei);
+
+        quicksort(arr, si, pidx-1);
+        quicksort(arr, pidx+1, ei);
+    }
+    public static void main(String[] args) {
+        int [] arr = {6,3,5,8,9};
+        int n = arr.length;
+
+        quicksort(arr, 0, n-1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }System.out.println();
+    }
+}
