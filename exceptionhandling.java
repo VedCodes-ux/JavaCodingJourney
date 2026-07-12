28b8fe6a47ef586dc7bcb2a78e83f6dbbc03e13e
VedCodes-ux <sablaniaved51@gmail.com>
2026-07-12 16:09:30 +0530

    learned excemption handling

---
+++ exceptionhandling.java
@@ -0,0 +1,43 @@
+
+class BookUnavailableException extends Exception{
+    public BookUnavailableException(String message){
+        super(message);
+    }
+}
+
+class InvalidMemberException extends Exception{
+    public InvalidMemberException(String message){
+        super(message);
+    }
+}
+class library{
+    int availablebooks = 3;
+    public void borrowbook(int bookrequestedcount,String ismember) throws Exception{
+        
+            if(ismember == null || ismember.trim().isEmpty()){
+                throw new InvalidMemberException("you are not a member");
+            }
+            if(bookrequestedcount <= 0){
+                throw new Exception("you must request a book");
+            }
+            if(bookrequestedcount > availablebooks){
+                throw new BookUnavailableException("not enough books");
+            }else{
+                System.out.println("book given");
+            }
+        }
+    }
+
+public class exceptionhandling {
+    public static void main(String[] args) {
+        try {
+        library l1 = new library();
+        l1.borrowbook(10,"Y");
+       } catch (Exception e) {
+        e.printStackTrace();
+        // TODO: handle exception
+        System.out.println("Exception occured " + e.getMessage());
+       }
+    }
+    
+}
